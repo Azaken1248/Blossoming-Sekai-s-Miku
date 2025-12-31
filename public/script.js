f6d088f0a056884800d612e9efaba7ea342f6da8
Azaken1248 <suvkrm@gmail.com>
2026-01-01 02:32:36 +0530

    fix issues

--- public/script.js
+++ public/script.js
@@ -3,10 +3,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
+            target.scrollIntoView({ block: 'start' });
         }
     });
 });
