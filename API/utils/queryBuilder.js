953c8cabc8aa1cf744b14d00c5a1917196e6b723
Azaken1248 <suvkrm@gmail.com>
2026-03-14 21:32:54 +0530

    Add a util for building data fetch query

---
+++ API/utils/queryBuilder.js
@@ -0,0 +1,37 @@
+export default class QueryBuilder {
+    constructor(mongooseQuery, queryString) {
+        this.query = mongooseQuery;
+        this.queryString = queryString;
+    }
+
+    filter() {
+        const queryObj = { ...this.queryString };
+        const excludedFields = ['page', 'sort', 'limit', 'fields', 'groupBy'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt|ne|in)\b/g, match => `$${match}`);
+        
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    sort() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-assignedAt');
+        }
+        return this;
+    }
+
+    paginate() {
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+        return this;
+    }
+}
