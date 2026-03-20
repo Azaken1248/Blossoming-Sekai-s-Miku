88f54b2bbf4d9c551729dc18610611fd0cec4c4f
Azaken1248 <suvkrm@gmail.com>
2026-03-21 04:58:01 +0530

    integrate with main API

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { createUser, getUser, addStrike, removeStrike } from '../controllers/userController.js';
+import { listUsers, createUser, getUser, getUserSummary, addStrike, removeStrike, generateShareCard } from '../controllers/userController.js';
 
 const router = express.Router();
 
+router.get('/', listUsers);
 router.post('/', createUser);
+
+router.get('/share/:discordId', generateShareCard);
+
+router.get('/:discordId/summary', getUserSummary);
 router.get('/:discordId', getUser);
 router.post('/:discordId/strikes/add', addStrike);
 router.post('/:discordId/strikes/remove', removeStrike);
