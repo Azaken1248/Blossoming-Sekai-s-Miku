8ad74c905427f71c35bd3b0dd288523099d438d7
Azaken1248 <suvkrm@gmail.com>
2026-03-22 00:09:26 +0530

    Switch to sharp

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { listUsers, createUser, getUser, getUserSummary, addStrike, removeStrike, generateShareCard, uploadCardImage, getCardImage } from '../controllers/userController.js';
+import { listUsers, createUser, getUser, getUserSummary, addStrike, removeStrike, generateShareCard, getCardImage } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -7,7 +7,6 @@ router.get('/', listUsers);
 router.post('/', createUser);
 
 router.get('/share/:discordId', generateShareCard);
-router.post('/:discordId/card', uploadCardImage);
 router.get('/:discordId/card.png', getCardImage);
 
 router.get('/:discordId/summary', getUserSummary);
